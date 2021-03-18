98b6c38aad72d1e6c94f886bd085dbab1120c072
Futursaith <saith_7@hotmail.fr>
2021-03-18 20:13:43 +0100

    sayMyName.

---
+++ sayMyName.js
@@ -0,0 +1,14 @@
+
+let prenom = process.argv[2];
+
+if (prenom) {
+  console.log(
+    `le prénom entré par l'utilisateur est ${prenom}`
+  );
+}
+
+else {
+  console.log('veuillez entrer un prénom svp')
+}
+
+
